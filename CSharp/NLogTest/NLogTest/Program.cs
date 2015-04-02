2d9ccea412c701f287fde96f34150f921705de65
blchen <blrchen@hotmail.com>
2015-04-02 19:14:20 +0800

    learn how to use NLog to print to both console and log file.

---
+++ CSharp/NLogTest/NLogTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Can you see me printed to both console and log file?");
+        }
+    }
+}
