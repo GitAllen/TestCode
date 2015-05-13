7e6ddf046cc1f1922943408e9946b2a843bc1c07
blchen <blrchen@hotmail.com>
2015-05-13 13:13:35 +0800

    use async

---
+++ Azure/TestAppendToBlobWithStream/TestAppendToBlobWithStream/BlobAppender.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data.Services.Client;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace TestAppendToBlobWithStream
+{
+    public class BlobAppender
+    {
+        private const int MaxBlockSize = 4 * 1024 * 1024;
+        private string _storageConnectionString;
+        private int _offset = 0;
+        private MemoryStream _currentStream = new MemoryStream();
+        private BlockingCollection<MemoryStream> _bufferQueue = new BlockingCollection<MemoryStream>();
+        private CloudBlockBlob _blob;
+
+        public BlobAppender(string storageConnectionString)
+        {
+            _storageConnectionString = storageConnectionString;
+        }
+
+        public void Run()
+        {
+            var storageAccount = CloudStorageAccount.Parse(_storageConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("mycontainer");
+            container.CreateIfNotExists();
+            _blob = container.GetBlockBlobReference("myblob.txt");
+            if (!_blob.Exists())
+            {
+                using (var s = new MemoryStream())
+                {
+                    _blob.UploadFromStream(s);
+                }
+            }
+
+            Task.Factory.StartNew(() => GenerateContentAsync());
+            Task.Factory.StartNew(() => AppendStreamToBlobAsync());
+            Task.WaitAll();
+        }
+
+
+        private async Task GenerateContentAsync()
+        {
+            int i = 0;
+            while (i < 10000000)
+            {
+                i++;
+                string line = string.Format("Content of line {0}{1}", i, Environment.NewLine);
+
+                var bytes = Encoding.Default.GetBytes(line);
+                await AddBufferToQueueAsync(bytes);
+            }
+        }
+
+        private async Task AddBufferToQueueAsync(byte[] bytes)
+        {
+            if (_offset + bytes.Length > MaxBlockSize)
+            {
+                var ms = new MemoryStream();
+                _currentStream.Position = 0;
+                await _currentStream.CopyToAsync(ms);
+                _bufferQueue.Add(ms);
+                Console.WriteLine("New buffer added to queue successfully");
+                _currentStream.SetLength(0);
+                _offset = 0;
+            }
+
+            await _currentStream.WriteAsync(bytes, 0, bytes.Length);
+            _offset += bytes.Length;
+        }
+
+        private async Task AppendStreamToBlobAsync()
+        {
+            foreach (var stream in _bufferQueue.GetConsumingEnumerable())
+            {
+                var blockIdList = new List<string>();
+                blockIdList.AddRange(_blob.DownloadBlockList().Select(b => b.Name));
+
+                var newBlockId = Convert.ToBase64String(Guid.NewGuid().ToByteArray());
+                stream.Position = 0;
+                await _blob.PutBlockAsync(newBlockId, stream, null);
+                blockIdList.Add(newBlockId);
+                await _blob.PutBlockListAsync(blockIdList);
+                stream.Close();
+
+                Console.WriteLine("Block {0} is appended successfully", newBlockId);
+            }
+        }
+    }
+}
--- Azure/TestAppendToBlobWithStream/TestAppendToBlobWithStream/Program.cs
+++ Azure/TestAppendToBlobWithStream/TestAppendToBlobWithStream/Program.cs
@@ -13,81 +13,17 @@ namespace TestAppendToBlobWithStream
 {
     class Program
     {
-        private const int MaxBlockSize = 4 * 1024 * 1024;
-        private static int _offset = 0;
-        private static MemoryStream _currentStream = new MemoryStream();
-        private static BlockingCollection<MemoryStream> _bufferQueue = new BlockingCollection<MemoryStream>();
-        private static CloudBlockBlob _blob;
+
 
         static void Main(string[] args)
         {
-            var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
-            var blobClient = storageAccount.CreateCloudBlobClient();
-            var container = blobClient.GetContainerReference("mycontainer");
-            container.CreateIfNotExists();
-            _blob = container.GetBlockBlobReference("myblob.txt");
-            if (!_blob.Exists())
-            {
-                using (var s = new MemoryStream())
-                {
-                    _blob.UploadFromStream(s);
-                }
-            }
-
-            Task.Factory.StartNew(GenerateContent);
-            Task.Factory.StartNew(AppendStreamToBlob);
-            Task.WaitAll();
+            string storageConnectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+            var blobAppender = new BlobAppender(storageConnectionString);
+            blobAppender.Run();
 
             Console.ReadLine();
         }
 
-        private static void GenerateContent()
-        {
-            int i = 0;
-            while (i < 100)
-            {
-                i++;
-                string line = string.Format("Content of line {0}{1}", i, Environment.NewLine);
 
-                var bytes = Encoding.Default.GetBytes(line);
-                AddBufferToQueue(bytes);
-            }
-        }
-
-        private static void AddBufferToQueue(byte[] bytes)
-        {
-            if (_offset + bytes.Length > MaxBlockSize)
-            {
-                var ms = new MemoryStream();
-                _currentStream.Position = 0;
-                _currentStream.CopyTo(ms);
-                _bufferQueue.Add(ms);
-                Console.WriteLine("New buffer added to queue successfully");
-                _currentStream.SetLength(0);
-                _offset = 0;
-            }
-
-            _currentStream.Write(bytes, 0, bytes.Length);
-            _offset += bytes.Length;
-
-        }
-
-        private static void AppendStreamToBlob()
-        {
-            foreach (var stream in _bufferQueue.GetConsumingEnumerable())
-            {
-                var blockIdList = new List<string>();
-                blockIdList.AddRange(_blob.DownloadBlockList().Select(b => b.Name));
-
-                var newBlockId = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace('+', 'A').Replace('\\', 'B').Replace('/', 'C');
-                stream.Position = 0;
-                _blob.PutBlock(newBlockId, stream, null);
-                blockIdList.Add(newBlockId);
-                _blob.PutBlockList(blockIdList);
-                stream.Close();
-
-                Console.WriteLine("Block {0} is appended successfully", newBlockId);
-            }
-        }
     }
 }
